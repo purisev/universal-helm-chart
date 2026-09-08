38fa4d939eb9bfbe617d953fee37556ebfbd4a76
purisev <purisev@users.noreply.github.com>
2026-09-08 08:55:44 +0000

    Deploy preview for PR 77 🛫

--- pr-preview/pr-77/searcher-09f2665d.js
+++ pr-preview/pr-77/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d5c2ccd4.js',
+                path_to_root + 'searchindex-d7b5505e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
